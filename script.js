4421b2c1057d47dad91c49321fe21802ece06c00
PrachiAgrwl <prachiagrawal504@gmail.com>
2022-07-11 07:16:28 -0700

    assessment

---
+++ script.js
@@ -0,0 +1,16 @@
+var namee=document.getElementById("Name");
+var rollno=document.getElementById("roll");
+var clgname=document.getElementById("ClgName");
+var date=document.getElementById("date");
+
+let n="Prachi Agrawal";
+let rn=201500480;
+let cn="GLA University,Mathura";
+let datee="11-07-2022"
+
+window.onload=function displayInfo(){
+   namee.innerText=n;
+   rollno.innerText=rn;
+   clgname.innerText=cn;
+   date.innerText=datee;
+}
